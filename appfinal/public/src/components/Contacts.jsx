c471c02902882b1f875a7d7f2bf613e409cc71eb
unknown <joaopereira.jp2002@gmail.com>
2022-07-04 09:34:41 +0100

    projeto

---
+++ appfinal/public/src/components/Contacts.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+
+export default function Contacts({ contacts, currentUser }) {
+    return <div>Contatos</div>;
+}
